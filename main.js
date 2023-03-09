8c900402378a8173c5d9fcde55c96c0b8829d8b8
Allyson Silva <allysonsilva091@gmail.com>
2023-03-09 20:38:01 +0000

    refactor: jquery

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
 
-
         const tarefa = $('#nome-tarefa').val();
-        const novaTarefa = $('<li></li>');
+        const novaTarefa = $('<li></li>').text(tarefa);
         $(tarefa).appendTo(novaTarefa);
-        $(novaTarefa).appendTo("ul");
+        novaTarefa.appendTo('dd');
 
         $('#nome-tarefa').val('');
+
+        $("dd").click(function() {
+            $(this).addClass("grifado");
+        });
     });
 });
